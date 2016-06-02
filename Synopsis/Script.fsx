b49fb90f9e5e88f4bb1ad7a6f394b289ffc8993c
Horia Toma <toma.horia@gmail.com>
2016-06-02 23:47:52 +0200

    Initial commit

---
+++ Synopsis/Script.fsx
@@ -0,0 +1,4 @@
+﻿#load "Synopsis.fs"
+open Helper
+
+splitTextInSentences @"What is he doing? I don't know him. What an answer! Well... Ask him"
---
+++ Synopsis/Synopsis.fs
@@ -0,0 +1,7 @@
+﻿module Helper
+
+open System
+open System.Text.RegularExpressions
+
+let splitTextInSentences text =
+    Regex.Split(text, @"(?<=([\.!\?])|(\.{3}))\s+")
