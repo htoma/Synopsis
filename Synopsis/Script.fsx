76507fb32353e7d537534bc66cdde11e048331b2
Horia Toma <toma.horia@gmail.com>
2016-06-04 19:36:15 +0200

    Common words sentence intersection

--- Synopsis/Script.fsx
+++ Synopsis/Script.fsx
@@ -1,4 +1,8 @@
 ﻿#load "Synopsis.fs"
 open Helper
 
-splitTextInSentences @"What is he doing? I don't know him. What an answer! Well... Ask him"
+open System
+
+let text = IO.File.ReadAllText(...)
+
+buildSynopsis text 10
--- Synopsis/Synopsis.fs
+++ Synopsis/Synopsis.fs
@@ -3,5 +3,36 @@
 open System
 open System.Text.RegularExpressions
 
-let splitTextInSentences text =
+let splitTextIntoSentences text =
     Regex.Split(text, @"(?<=([\.!\?])|(\.{3}))\s+")
+
+let splitSentenceIntoWords (sen: string) =
+    sen.Split(@" .,;-!?:()""" |> Array.ofSeq, StringSplitOptions.RemoveEmptyEntries)
+
+let intersect first second =
+    let wordsFirst = splitSentenceIntoWords first
+                        |> Array.map (fun w -> w.ToLower())
+                        |> Set.ofArray
+
+    let wordsSecond = splitSentenceIntoWords second
+                        |> Array.map (fun w -> w.ToLower())
+                        |> Set.ofArray
+
+    match wordsFirst.Count,wordsSecond.Count with
+    | (0,_) | (_,0) -> 0
+    | _ ->
+        let common = wordsFirst
+                        |> Set.filter (fun w -> wordsSecond |> Set.contains w)
+                        |> Set.count
+
+        common / ((wordsFirst.Count+wordsSecond.Count)/2)
+
+let buildSynopsis (text: string) (sentenceCount: int) =
+    let sentences = splitTextIntoSentences text
+    sentences
+    |> Array.mapi (fun i s -> i,s,(sentences
+                                   |> Array.fold (fun state t -> state+(intersect s t)) -1))
+    |> Array.sortByDescending (fun (_,_,score) -> score)
+    |> Array.take sentenceCount
+    |> Array.sortBy (fun (i,_,_) -> i)
+    |> Array.map (fun (_,s,_) -> s)
